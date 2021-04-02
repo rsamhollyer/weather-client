8af95014014863bc24a421e41a78c1c7fb6c40be
rsamhollyer <rsamhollyer@gmail.com>
2021-04-02 12:22:03 -0500

    updated axios call to point at the backend, not the api route

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import CitySearch from "./components/CitySearch";
 import Weather from "./components/Weather";
+
 function App() {
   // State
   const [inputText, setInputText] = useState("");
   const [cityName, setCityName] = useState({});
   const [weather, setWeather] = useState({});
+  const [savedSearches, setSavedSearches] = useState([]);
 
   //Axios
 
   const getWeather = async (str) => {
-    const URL = `https://api.openweathermap.org/data/2.5/weather`;
+    const URL = `/api/weather`;
     try {
       const resp = await axios.get(URL, {
         params: {
           q: str,
-          appid: process.env.REACT_APP_WEATHER_KEY,
           units: "imperial",
         },
       });
