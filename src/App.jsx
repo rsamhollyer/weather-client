bf1110c1bb8212f1f668101c205694887a52b1bc
rsamhollyer <rsamhollyer@gmail.com>
2021-04-02 08:26:49 -0500

    updated app script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import axios from "axios";
+import CitySearch from "./components/CitySearch";
+
 function App() {
+  // State
+  const [cityName, setCityName] = useState("");
   return (
     <div className="App">
       <h1>WEATHER APP</h1>
+      <CitySearch cityName={cityName} setCityName={setCityName} />
     </div>
   );
 }
