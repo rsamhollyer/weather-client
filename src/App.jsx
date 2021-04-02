09bc199bd5f120038c43eae03eebd411b856acf6
rsamhollyer <rsamhollyer@gmail.com>
2021-04-02 09:03:35 -0500

    added more state and an axios call

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,28 @@ import CitySearch from "./components/CitySearch";
 
 function App() {
   // State
-  const [cityName, setCityName] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [cityName, setCityName] = useState({});
+  const [weather, setWeather] = useState({});
+
+  //Axios
+
+  const getWeather = async (obj) => {
+    const URL = `api.openweathermap.org/data/2.5/weather?`;
+    const data = await axios.get(URL, {
+      params: { q: cityName, appid: "6a52720ab8e519a37c28b8c621021acf" },
+    });
+    setWeather(data.data);
+  };
+
   return (
     <div className="App">
       <h1>WEATHER APP</h1>
-      <CitySearch cityName={cityName} setCityName={setCityName} />
+      <CitySearch
+        setCityName={setCityName}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
     </div>
   );
 }
